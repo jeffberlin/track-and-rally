c93b6e70b9f41c8e5a2cbcc010d408baef02409c
Jeff Berlin <jeff@jbrally.com>
2020-05-27 10:04:05 -0400

    Adding Helmet / HSTS

---
+++ build/precache-manifest.869253be420f5cd8713307e70bd2c08a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e232b755f446fe5bd2048dbf5875b825",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec9de17d5b7d0617b892",
+    "url": "/static/css/2.9ff99f11.chunk.css"
+  },
+  {
+    "revision": "dab68df9274e713a0fbd",
+    "url": "/static/css/main.a958c536.chunk.css"
+  },
+  {
+    "revision": "ec9de17d5b7d0617b892",
+    "url": "/static/js/2.b483622c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.b483622c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dab68df9274e713a0fbd",
+    "url": "/static/js/main.e15566cb.chunk.js"
+  },
+  {
+    "revision": "f7142cbf7bba7ea677cb",
+    "url": "/static/js/runtime-main.7567358b.js"
+  }
+]);
