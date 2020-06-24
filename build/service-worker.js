d0babbf8d3d08a82bcdd1ad4ad33ecdab7e2fd57
Jeff Berlin <jeff@jbrally.com>
2020-06-24 12:26:11 -0400

    Fixing open graph, compression

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.85d4dfcb751f872d801ed7c8a5ddc9f5.js"
+  "/precache-manifest.6c679e2132682c7fc7f55228300a86cf.js"
 );
 
 self.addEventListener('message', (event) => {
